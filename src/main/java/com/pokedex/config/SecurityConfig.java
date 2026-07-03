df8d7b35e9f667977c9a4106059e18cb9756f8a1
NicolasPrieto12 <nicolas.prieto-r@mail.escuelaing.edu.co>
2026-07-02 23:41:49 -0500

    fix: UserDetailsService bean para suprimir auto-configuracion de Spring Security

--- src/main/java/com/pokedex/config/SecurityConfig.java
+++ src/main/java/com/pokedex/config/SecurityConfig.java
@@ -8,6 +8,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -35,5 +38,10 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager();
     }
 }
