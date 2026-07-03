dc5b0f103120c6bd0b4465d9a730d8a6c94d1c14
NicolasPrieto12 <nicolas.prieto-r@mail.escuelaing.edu.co>
2026-07-02 23:35:40 -0500

    fix: EnableJpaRepositories y EnableMongoRepositories explícitos

--- src/main/java/com/pokedex/api/PokedexApiApplication.java
+++ src/main/java/com/pokedex/api/PokedexApiApplication.java
@@ -2,8 +2,12 @@ package com.pokedex.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.pokedex")
+@EnableJpaRepositories(basePackages = "com.pokedex.persistence.repository.relational")
+@EnableMongoRepositories(basePackages = "com.pokedex.persistence.repository.document")
 public class PokedexApiApplication {
 
 	public static void main(String[] args) {
