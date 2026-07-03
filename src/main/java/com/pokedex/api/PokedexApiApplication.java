09a4aeb515f3e6d406a66f82d12987cf2fb6a716
NicolasPrieto12 <nicolas.prieto-r@mail.escuelaing.edu.co>
2026-07-02 23:31:47 -0500

    fix: scanBasePackages com.pokedex para detectar repositorios y SecurityConfig

--- src/main/java/com/pokedex/api/PokedexApiApplication.java
+++ src/main/java/com/pokedex/api/PokedexApiApplication.java
@@ -3,7 +3,7 @@ package com.pokedex.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.pokedex")
 public class PokedexApiApplication {
 
 	public static void main(String[] args) {
